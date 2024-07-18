1a064cd714b8cb23517ec6d000285f5e062d5c54
PALLADIUM26 <pranithdutta26@gmail.com>
2024-07-18 10:44:06 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -11,16 +11,25 @@
 
 #     return [Output(result, "my_output", party3)]
 
-from nada_dsl import *
+# from nada_dsl import *
 
+# def nada_main():
+#     party1 = Party(name="Party1")
+#     A = SecretInteger(Input(name="A", party=party1))
+#     B = SecretInteger(Input(name="B", party=party1))
+#     C = SecretInteger(Input(name="C", party=party1))
+
+#     TMP1 = A + B
+#     D = TMP1 * C
+
+#     return [Output(D, "D", party1)]
+
+from nada_dsl import *
 def nada_main():
     party1 = Party(name="Party1")
-    A = SecretInteger(Input(name="A", party=party1))
-    B = SecretInteger(Input(name="B", party=party1))
-    C = SecretInteger(Input(name="C", party=party1))
-
-    TMP1 = A + B
-    D = TMP1 * C
-
-    return [Output(D, "D", party1)]
-
+    v = SecretInteger(Input(name="v", party=party1))
+    f = SecretInteger(Input(name="f", party=party1))
+    th = SecretInteger(Input(name="th", party=party1))
+    n = SecretInteger(Input(name="n", party=party1))
+    c = v*(f-th)/n
+    return [Output(c, "c", party1)]
